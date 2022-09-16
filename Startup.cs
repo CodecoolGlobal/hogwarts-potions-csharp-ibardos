1fc7fd50e1ce2ad5adc89d8470cc9352335e3016
Bárdos István <bardos.istvan@icloud.com>
2022-09-16 23:05:18 +0200

    Switch DB configuration to PSQL (Npgsql)

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,7 @@ namespace HogwartsPotions
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<HogwartsContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
         }
