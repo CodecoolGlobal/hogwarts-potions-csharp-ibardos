9e5d1b98a17fec8814a5bf225a55e3a72a867861
Bárdos István <bardos.istvan@icloud.com>
2022-09-17 16:23:34 +0200

    Create methods to add resident/residents into Room types

--- Models/Entities/Room.cs
+++ Models/Entities/Room.cs
@@ -16,5 +16,26 @@ namespace HogwartsPotions.Models.Entities
         {
             Capacity = capacity;
         }
+
+        /// <summary>
+        /// Adds one resident into room.
+        /// </summary>
+        /// <param name="resident"></param>
+        public void AddResident(Student resident)
+        {
+            Residents.Add(resident);
+        }
+
+        /// <summary>
+        /// Adds all of the residents from a Hashset into room.
+        /// </summary>
+        /// <param name="residents"></param>
+        public void AddResidents(HashSet<Student> residents)
+        {
+            foreach (Student resident in residents)
+            {
+                AddResident(resident);
+            }
+        }
     }
 }
