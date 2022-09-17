b8e3b3bbabc39785c0e8bb3c4f2bfed91bcb3d43
Bárdos István <bardos.istvan@icloud.com>
2022-09-17 17:12:46 +0200

    Check if SQL tables are exist
    If no, create them. If yes, no action.

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
 
 namespace HogwartsPotions.Data;
 
@@ -7,6 +10,13 @@ public static class DbInitializer
     public static void Initialize(HogwartsContext context)
     {
         context.Database.EnsureCreated();
+
+        // Check if SQL tables are exist, if so, no action needed
+        if (context.Students.Any() || context.Rooms.Any())
+        {
+            return;
+        }
+        
         
         // Initial data addition comes here
         // context.SaveChanges();
