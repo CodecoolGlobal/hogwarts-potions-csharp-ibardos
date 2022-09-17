2c07c2ee2b5a52e4ffaa461ba84676e9e8271ed6
Bárdos István <bardos.istvan@icloud.com>
2022-09-17 17:13:48 +0200

    Add Students and Rooms into SQL database
    Initial DB creation

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -17,8 +17,48 @@ public static class DbInitializer
             return;
         }
         
+        // Initial data addition
         
-        // Initial data addition comes here
-        // context.SaveChanges();
+        // Data creation (Student/Room)
+        // Students
+        Student harry = new Student("Harry Potter", HouseType.Gryffindor, PetType.Owl);
+        Student hermione = new Student("Hermione Granger", HouseType.Gryffindor, PetType.Cat);
+        Student ron = new Student("Ron Weasley", HouseType.Gryffindor, PetType.Rat);
+        Student draco = new Student("Draco Malfoy", HouseType.Slytherin, PetType.Owl);
+        HashSet <Student> students = new HashSet<Student>() {harry, hermione, ron, draco};
+
+        // Rooms
+        Room room1 = new Room(5);
+        Room room2 = new Room(5);    
+        Room room3 = new Room(5);
+        Room room4 = new Room(5);
+        Room room5 = new Room(5);
+        HashSet<Room> rooms = new HashSet<Room>(){room1, room2, room3, room4, room5};
+
+        // Assignments (Student-Room / Room-Residents)
+        // Student-Room
+        harry.ChooseRoom(room1);
+        hermione.ChooseRoom(room1);
+        ron.ChooseRoom(room1);
+        draco.ChooseRoom(room2);
+        
+        // Room-Residents
+        room1.AddResidents(new HashSet<Student> {harry, hermione, ron});
+        room2.AddResident(draco);
+        
+        // Add to SQL database
+        // Add Students
+        foreach (Student student in students)
+        {
+            context.Students.Add(student);
+        }
+        
+        // Add Rooms
+        foreach (Room room in rooms)
+        {
+            context.Rooms.Add(room);
+        }
+        
+        context.SaveChanges();
     }
 }
