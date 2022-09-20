595b66003443ec3940bb2df9649d239705cf3737
Bárdos István <bardos.istvan@icloud.com>
2022-09-20 10:39:06 +0200

    Create RoomService.cs

---
+++ Services/Implementations/RoomService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Services.Implementations;
+
+public class RoomService : IRoomService
+{
+}
