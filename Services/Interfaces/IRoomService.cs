d28d49c2761a4caf21dfeb58e2cf97a803b5a7c6
Bárdos István <bardos.istvan@icloud.com>
2022-09-20 10:38:21 +0200

    Define CRUD operations in IRoomService.cs, without implementations

--- Services/Interfaces/IRoomService.cs
+++ Services/Interfaces/IRoomService.cs
@@ -6,4 +6,19 @@ namespace HogwartsPotions.Services.Interfaces;
 
 public interface IRoomService
 {
+    // CRUD operations
+    
+    // Create
+    Task AddRoom(Room room);
+    
+    // Read
+    Task<Room> GetRoomById(long roomId);
+    Task<IEnumerable<Room>> GetAllRooms();
+    
+    // Update
+    Task UpdateRoom(Room room);
+    
+    // Delete
+    Task DeleteRoom(Room room);
+    Task<List<Room>> GetRoomsForRatOwners();
 }
