a3fef1440e81fb4dd0c071a764658e971a30fe41
Bárdos István <bardos.istvan@icloud.com>
2022-09-20 10:42:00 +0200

    Delete CRUD methods from database context to move them into a service
    CRUD operations will be defined (with implementations) in a service layer.

--- Data/HogwartsContext.cs
+++ Data/HogwartsContext.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using HogwartsPotions.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +5,6 @@ namespace HogwartsPotions.Data
 {
     public class HogwartsContext : DbContext
     {
-        public const int MaxIngredientsForPotions = 5;
         public DbSet<Student> Students { get; set; }
         public DbSet<Room> Rooms { get; set; }
 
@@ -17,38 +12,7 @@ namespace HogwartsPotions.Data
         {
         }
 
-        public async Task AddRoom(Room room)
         {
-            Rooms.Add(room);
-        }
-
-        public Task<Room> GetRoom(long roomId)
-        {
-            return Rooms
-                .Include(room => room.Residents)
-                .FirstAsync(room => room.ID == roomId);
-        }
-
-        public Task<List<Room>> GetAllRooms()
-        {
-            return Rooms
-                .Include(room => room.Residents)
-                .ToListAsync();
-        }
-
-        public async Task UpdateRoom(Room room)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task DeleteRoom(long id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Room>> GetRoomsForRatOwners()
-        {
-            throw new NotImplementedException();
         }
     }
 }
