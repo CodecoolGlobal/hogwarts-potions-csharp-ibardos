110ae193df0e188e349419a2e31775f59b42b284
Bárdos István <bardos.istvan@icloud.com>
2022-09-25 13:49:58 +0200

    Add properties for Recipe type

--- Models/Entities/Recipe.cs
+++ Models/Entities/Recipe.cs
@@ -5,4 +5,10 @@ namespace HogwartsPotions.Models.Entities;
 
 public class Recipe
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+
+    public string Name { get; set; }
+    public Student Student { get; set; }
+    public List<Ingredient> Ingredients { get; set; }
 }
