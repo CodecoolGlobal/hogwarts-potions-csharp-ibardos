de622650e8717263e3e2cd450f7eacea495ccd7e
Bárdos István <bardos.istvan@icloud.com>
2022-09-25 17:59:31 +0200

    Refactor formatting at DbInitializer.cs

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -16,41 +16,50 @@ public static class DbInitializer
         {
             return;
         }
-        
-        // Initial data addition
-        
+
+
+        // INITIAL DATA ADDITION
+
         // Data creation (Student/Room)
         // Students
         Student harry = new Student("Harry Potter", HouseType.Gryffindor, PetType.Owl);
         Student hermione = new Student("Hermione Granger", HouseType.Gryffindor, PetType.Cat);
         Student ron = new Student("Ron Weasley", HouseType.Gryffindor, PetType.Rat);
         Student draco = new Student("Draco Malfoy", HouseType.Slytherin, PetType.Owl);
-        HashSet <Student> students = new HashSet<Student>() {harry, hermione, ron, draco};
+        HashSet<Student> students = new HashSet<Student>() { harry, hermione, ron, draco };
 
         // Rooms
         Room room1 = new Room(5);
-        Room room2 = new Room(5);    
+        Room room2 = new Room(5);
         Room room3 = new Room(5);
         Room room4 = new Room(5);
         Room room5 = new Room(5);
-        HashSet<Room> rooms = new HashSet<Room>(){room1, room2, room3, room4, room5};
+        HashSet<Room> rooms = new HashSet<Room>() { room1, room2, room3, room4, room5 };
 
-        // Assignments (Student-Room / Room-Residents)
+        // Ingredients
+
+        // Recipes
+
+        // Potions
+
+
+        // Assignment (Student-Room / Room-Residents)
         // Student-Room
         harry.ChooseRoom(room1);
         hermione.ChooseRoom(room1);
         ron.ChooseRoom(room1);
         draco.ChooseRoom(room2);
-        
+
         // Room-Residents
-        room1.AddResidents(new HashSet<Student> {harry, hermione, ron});
+        room1.AddResidents(new HashSet<Student> { harry, hermione, ron });
         room2.AddResident(draco);
-        
+
+
         // Add to SQL database
         // Add Students
         context.Students.AddRange(students);
         context.SaveChanges();
-        
+
         // Add Rooms
         context.Rooms.AddRange(rooms);
         context.SaveChanges();
