7f8193e8972544b57238ae96fcab7383f30102ca
Bárdos István <bardos.istvan@icloud.com>
2022-09-26 17:59:25 +0200

    Define properties for Potion type

--- Models/Entities/Potion.cs
+++ Models/Entities/Potion.cs
@@ -6,4 +6,18 @@ namespace HogwartsPotions.Models.Entities;
 
 public class Potion
 {
+    public const int MaxIngredientsForPotions = 5;
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+
+    public string Name { get; set; }
+    public Student Student { get; set; }
+    public HashSet<Ingredient> Ingredients { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public BrewingStatus BrewingStatus { get; set; }
+
+    public Recipe Recipe { get; set; }
+
 }
