f6e4839501730f9bed3063403b7fbf42a22d8239
Bárdos István <bardos.istvan@icloud.com>
2022-09-26 18:13:31 +0200

    Create PotionApiController.cs

---
+++ Controllers/PotionApiController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers;
+
+[ApiController, Route("api/potion")]
+public class PotionApiController : ControllerBase
+{
+}
