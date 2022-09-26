aa7891a0a9bdf8dd65a9c2c7f9b0c210b7300f64
Bárdos István <bardos.istvan@icloud.com>
2022-09-26 17:50:47 +0200

    Add navigation properties to Ingredient model
    Navigation properties were necessary  to create additional tables to connect Ingredient table to Potion and Recipe tables.

--- Models/Entities/Ingredient.cs
+++ Models/Entities/Ingredient.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HogwartsPotions.Models.Entities;
@@ -6,7 +7,12 @@ public class Ingredient
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long ID { get; set; }
+
     public string Name { get; set; }
+
+    // Navigation properties to establish connection to other tables in SQL DB
+    public HashSet<Potion> Potions { get; set; }
+    public HashSet<Recipe> Recipes { get; set; }
 
     public Ingredient(string name)
     {
