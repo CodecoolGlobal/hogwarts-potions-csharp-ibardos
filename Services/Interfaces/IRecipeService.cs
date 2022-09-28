7f07e7af899069e10b04dcf7ab64818ce4c2592d
Bárdos István <bardos.istvan@icloud.com>
2022-09-28 23:58:30 +0200

    Add CRUD operations to IRecipeService without implementations

--- Services/Interfaces/IRecipeService.cs
+++ Services/Interfaces/IRecipeService.cs
@@ -6,4 +6,22 @@ namespace HogwartsPotions.Services.Interfaces;
 
 public interface IRecipeService
 {
+    // CRUD operations - Entity Framework Core
+    // Create
+    Task AddRecipe(Recipe recipe);
+
+    // Read
+    Task<Recipe> GetRecipeByIngredients(HashSet<Ingredient> ingredients);
+    Task<List<Recipe>> GetRecipesByStudent(Student student);
+    Task<int> GetNumberOfRecipesByStudent(Student student);
+
+    // Update
+
+
+    // Delete
+    
+    
+    
+    // Helper methods
+    Recipe CreateRecipe(Student student, HashSet<Ingredient> ingredients, int studentsNextRecipeNumber);
 }
