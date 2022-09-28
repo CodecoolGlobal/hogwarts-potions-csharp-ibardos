7b2c617ede9343540dc6356a39f92f4b118d837d
Bárdos István <bardos.istvan@icloud.com>
2022-09-28 23:58:55 +0200

    Create RecipeService.cs

---
+++ Services/Implementations/RecipeService.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Services.Implementations;
+
+public class RecipeService : IRecipeService
+{
+}
