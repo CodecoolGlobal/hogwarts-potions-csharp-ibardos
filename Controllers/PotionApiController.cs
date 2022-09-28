2357a679b4f0ed150f0990b819ea19b89ee98604
Bárdos István <bardos.istvan@icloud.com>
2022-09-29 00:09:14 +0200

    Update dependecies at PotionApiController

--- Controllers/PotionApiController.cs
+++ Controllers/PotionApiController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
 using HogwartsPotions.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,18 @@ namespace HogwartsPotions.Controllers;
 public class PotionApiController : ControllerBase
 {
     private readonly IPotionService _potionService;
+    private readonly IStudentService _studentService;
+    private readonly IRecipeService _recipeService;
 
-    public PotionApiController(IPotionService potionService)
+    public PotionApiController(
+        IPotionService potionService,
+        IStudentService studentService,
+        IRecipeService recipeService
+    )
     {
         _potionService = potionService;
+        _studentService = studentService;
+        _recipeService = recipeService;
     }
 
     [HttpGet]
