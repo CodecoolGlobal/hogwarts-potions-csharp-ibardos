9b79ab1712cf7efca17dc40155b0cdbbdeeaef2a
Bárdos István <bardos.istvan@icloud.com>
2022-09-29 00:09:33 +0200

    Implement AddPotion api endpoint

--- Controllers/PotionApiController.cs
+++ Controllers/PotionApiController.cs
@@ -30,4 +30,42 @@ public class PotionApiController : ControllerBase
     {
         return await _potionService.GetAllPotions();
     }
+
+    [HttpPost("{studentId}")]
+    public async Task<IActionResult> AddPotion(long studentId, [FromBody] Potion potionFromStudent)
+    {
+        Student student = await _studentService.GetStudentById(studentId);
+
+        // Check if Student is existing in DB with the defined studentId
+        if (student is null)
+        {
+            return NotFound($"Student with studentId: {studentId}, does not exist in the database.");
+        }
+
+        // Numbering used during Recipe & Potion name generation
+        int studentsNextRecipeNumber = await _recipeService.GetNumberOfRecipesByStudent(student) + 1;
+        int studentsNextPotionNumber = await _potionService.GetNumberOfPotionsByStudent(student) + 1;
+
+
+        Recipe recipeFromDb = await _recipeService.GetRecipeByIngredients(potionFromStudent.Ingredients);
+
+        // If a Recipe with the same Ingredients exists in DB
+        if (recipeFromDb is not null)
+        {
+            // Save Potion as Replica with already existing Recipe
+            Potion potionReplica = _potionService.CreatePotion(student, BrewingStatus.Replica, recipeFromDb, studentsNextPotionNumber);
+            await _potionService.AddPotion(potionReplica);
+            return CreatedAtAction(nameof(AddPotion), potionReplica);
+        }
+
+        // If Recipe with the same Ingredients does NOT exists in DB
+        // Create new recipe based on Student's Ingredients
+        Recipe recipeFromStudent = _recipeService.CreateRecipe(student, potionFromStudent.Ingredients, studentsNextRecipeNumber);
+
+        // Save Potion to DB as Discovery
+        Potion potionDiscovery = _potionService.CreatePotion(student, BrewingStatus.Discovery, recipeFromStudent, studentsNextPotionNumber);
+        await _potionService.AddPotion(potionDiscovery);
+        
+        return CreatedAtAction(nameof(AddPotion), potionDiscovery);
+    }
 }
