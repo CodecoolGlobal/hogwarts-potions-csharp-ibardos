3636417d88ac9625902a4d6e786da728d488f581
Bárdos István <bardos.istvan@icloud.com>
2022-09-29 00:02:57 +0200

    Create StudentService.cs

---
+++ Services/Implementations/StudentService.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using HogwartsPotions.Data;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Services.Implementations;
+
+public class StudentService : IStudentService
+{
+}
