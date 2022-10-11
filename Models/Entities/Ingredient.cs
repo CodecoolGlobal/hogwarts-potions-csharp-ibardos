42086f96e34a0021c4a22bad76605d603c9ae6b0
Bárdos István <bardos.istvan@icloud.com>
2022-10-12 01:01:41 +0200

    Ignore navigation properties during JSON serialization
    Navigation properties should be ignored during JSON serialization, as they make API response noisy with their tremendous amount of data.

--- Models/Entities/Ingredient.cs
+++ Models/Entities/Ingredient.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace HogwartsPotions.Models.Entities;
 
@@ -11,7 +12,9 @@ public class Ingredient
     public string Name { get; set; }
 
     // Navigation properties to establish connection to other tables in SQL DB
+    [JsonIgnore]
     public HashSet<Potion> Potions { get; set; }
+    [JsonIgnore]
     public HashSet<Recipe> Recipes { get; set; }
 
     public Ingredient(string name)
